9c7b4d733d81d3ed45e3b946c0aa9b2e75e4d939
Faisal Hassan <faisal.hassanx@gmail.com>
2017-05-02 00:34:36 +0000

    Add flash notice and edit restful foutes for contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  resources :contacts, :only => [:new]
+  resources :contacts, :only => [:new, :create]
   
   get 'about' => 'pages#about'
   root 'pages#home'
