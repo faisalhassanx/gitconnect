664cb13cee44bf179f4c88d70b427581e48f3c4a
Faisal Hassan <faisal.hassanx@gmail.com>
2017-05-08 02:52:39 +0000

    Change body to message

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
     if @contact.save
       name = params[:contact][:name]
       email = params[:contact][:email]
-      body = params[:contact][:message]
+      message = params[:contact][:message]
       ContactMailer.contact_email(name, email, body).deliver
       flash[:success] = "Message sent."
       redirect_to root_path
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,9 @@
 class ContactMailer < ActionMailer::Base
   default to: 'faisal.hassanx@gmail.com'
-  def contact_email(name, email, body)
+  def contact_email(name, email, message)
     @name = name
     @email = email
     @message = message
-    mail(from: email, subject: 'Contact Form Message from GitConnect')
+    mail(from: email, subject: 'Contact Form Message from GitConnect', message)
   end
 end
