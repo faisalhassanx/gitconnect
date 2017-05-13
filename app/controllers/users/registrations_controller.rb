23492c360e258af5647dc57ebbabe8c0f3bc44c1
Faisal Hassan <faisal.hassanx@gmail.com>
2017-05-13 01:49:15 +0000

    Add code comments for Stripe

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  # Extend default devise gem behavior so that
+  # users signing up from Pro account(plan id=2)
+  # save with a special Stripe subscription function
+  # Else devise signs up with basic form
   def create
     super do |resource|
       if params[:plan]
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
   belongs_to :plan
   
   attr_accessor :stripe_card_token
+  # If Pro user passes form validations
+  # then set up subscription with Stripe.
+  # Stripe responds with customer data
+  # Store customer.id as customer token and save user
   def save_with_subscription
     if valid?
       customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
