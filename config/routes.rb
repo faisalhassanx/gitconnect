c04483f20c35a8624636d1e062054b9788cf7a0a
Faisal Hassan <faisal.hassanx@gmail.com>
2017-05-15 02:48:45 +0000

    Add routes for user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   
+  resources :users do
+    resource :profile
+  end
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :contacts, :only => [:create]
   get 'contact' => 'contacts#new', as: 'new_contact'
