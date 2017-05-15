7c6ba1480fded2bbbca15498fcdbb72c60c2ee4a
Faisal Hassan <faisal.hassanx@gmail.com>
2017-05-15 22:25:56 +0000

    Add profiles controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   resources :users do
     resource :profile
   end
+  
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :contacts, :only => [:create]
+  
   get 'contact' => 'contacts#new', as: 'new_contact'
   
   get 'about' => 'pages#about'
