6f5effea2bcf278575f364ccefb5050322838df1
Faisal Hassan <faisal.hassanx@gmail.com>
2017-05-17 02:06:36 +0000

    Add validations to update profile form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,5 +4,26 @@ class ProfilesController < ApplicationController
     @profile = Profile.new
   end
   
+  # POST to /users/:user_id/profile
+  def create
+    # Ensure we have a user who is filling out the form
+    @user = User.find(params[:user_id])
+    # Create profile linked to this specific user
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = "Profile updated"
+      redirect_to root_path
+    else
+      flash[:danger] = "Please try again"
+      render action: :new
+    end
+  end
+  
+  private
+  
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :job_title, :company,
+                                    :phone_number, :contact_email, :about)
+  end
   
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,15 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
   
+  validates :first_name,    presence:   true, length: { maximum: 50 }
+  validates :last_name,     presence:   true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
+  validates :email,   presence:   true, 
+                      length:     { maximum: 255 },
+                      format:     { with: VALID_EMAIL_REGEX },
+                      uniqueness: { case_sensitive: false }
+  validates :job_title,     presence:   true, length: { maximum: 50 }
+  validates :company,       length: { maximum: 50 }
+  validates :about,         presence:   true, length: { maximum: 500 }
+  
 end
