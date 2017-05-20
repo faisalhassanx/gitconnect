f96ae76993ba20df18cfa3b27a005678efc47b41
Faisal Hassan <faisal.hassanx@gmail.com>
2017-05-20 01:40:36 +0000

    Add authentication filters to restrict access

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
   # GET to /users/:user_id/profile/new
   def new
     @profile = Profile.new
@@ -41,6 +43,11 @@ class ProfilesController < ApplicationController
   def profile_params
     params.require(:profile).permit(:first_name, :last_name, :job_title, :company, 
                                     :phone_number, :contact_email, :about, :avatar)
+  end
+  
+  def only_current_user
+    @user = User.find(params[:user_id])
+    redirect_to(root_url) unless @user == current_user
   end
   
 end
